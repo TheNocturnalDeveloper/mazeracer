bf3b721a290bab75563041eae1dc3cf9d13960ed
Jake <jakevanhout@outlook.com>
2020-01-22 09:10:31 +0100

    Add interface documentation
    Add interface documentation.

--- data/src/main/java/nl/fontys/se3/data/IUserDAO.java
+++ data/src/main/java/nl/fontys/se3/data/IUserDAO.java
@@ -3,9 +3,43 @@ package nl.fontys.se3.data;
 import java.util.List;
 
 public interface IUserDAO {
+
+    /**
+     * fetches all of the users.
+     *
+     * @return a list of all the users.
+     */
     List<UserDTO> getAllUsers();
+
+    /**
+     * returns a player based on their username
+     *
+     * @param username the name of the user that will be returned
+     * @return a user
+     */
     UserDTO getPlayerByName(String username);
+
+    /**
+     * Reads given resource file as a string.
+     *
+     * @param username the name of the user whose score will be updated
+     * @param score the new score of the player
+     */
     void updatePlayerScore(String username, int score);
+
+    /**
+     * inserts a new player
+     *
+     * @param player the player that will be inserted
+     */
     void insertPlayer(UserDTO player);
+
+    /**
+     * checks the provided credentials and returns the player
+     *
+     * @param username the username of the player that will be checked
+     * @param password the password of the player that will be checked
+     * @return the player that matched with the credentials
+     */
     UserDTO checkCredentials(String username, String password);
 }
--- logic/src/main/java/nl/fontys/se3/logic/IMazeRacerClient.java
+++ logic/src/main/java/nl/fontys/se3/logic/IMazeRacerClient.java
@@ -1,19 +1,56 @@
 package nl.fontys.se3.logic;
 
+
 public interface IMazeRacerClient {
 
+    /**
+     * adds an enemy to the client. the name will be used when moving or removing the player.
+     *
+     * @param username the name of the user that joined
+     */
     void addEnemy(String username);
 
+    /**
+     * moves an enemy to a specific coordinate
+     *
+     * @param username the username of the enemy that will be moved
+     * @param coord the coordinate where the player will be moved
+     */
     void moveEnemy(String username, Coord coord);
 
+    /**
+     * notifies the client which player has won
+     *
+     * @param username the name of the user that joined
+     */
     void notifyPlayerWon(String username);
 
+    /**
+     * removes a bonus from the client
+     *
+     * @param coord the coordinate of the bonus that will be removed
+     */
     void removeBonus(Coord coord);
 
+    /**
+     * Resets the client to a specific coordinate
+     *
+     * @param coord the new position of the client
+     */
     void resetPosition(Coord coord);
 
+    /**
+     * Reads given resource file as a string.
+     *
+     * @param cells the name of the user that joined
+     */
     void loadMaze(Cell[] cells);
 
+    /**
+     * Removes an enemy from the client
+     *
+     * @param username the name of the user that left
+     */
     void removeEnemy(String username);
 
 }
